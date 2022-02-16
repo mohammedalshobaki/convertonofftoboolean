b8a57d4ee46d53bb7d814b0662703d38804bd18b
Mohammed Alshobaki <mohammed.alshobaki96@gmail.com>
2022-02-16 11:15:19 +0200

    first commit

---
+++ helper.php
@@ -0,0 +1,21 @@
+/*
+ * Function description: Helper function to convert every 'on' / 'off' to boolean when using check inputs, input name must start with 'is_', Laravel 8.x
+ * Call by reference 
+ * Created by VS code
+ * User: Mohammed Alshobaki
+ * Email: mohammed.alshobaki96@gmail.com
+ * Date: 16/02/22
+ * Time: 10.00 AM
+ */
+
+function convertOnOffToBoolean(&$data, $originalKeys)
+{
+    foreach($data  as $key => $value) 
+    if($value == 'on')$data[$key]=true;
+
+    $differenceKeys = array_diff_key($originalKeys,$data);
+    foreach ($differenceKeys as $key => $value):
+        if (str_starts_with($key, 'is_')) 
+            $data[$key]=false;
+    endforeach;
+}
