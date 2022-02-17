11390140f496c1c3762a3720bb14024e69106379
Othman Adlan <othman.aladlan@makane.com>
2022-02-18 01:50:52 +0200

    fix indentation of code.

--- helper.php
+++ helper.php
@@ -10,20 +10,20 @@
  * Time: 10.00 AM
  */
 
- if (! function_exists('convertOnOffToBoolean')) {
-     function convertOnOffToBoolean(&$data, $originalKeys)
-     {
-         foreach ($data as $key => $value):
-             if (strtolower($value) == 'on') {
-                 $data[$key] = true;
-             }
-         endforeach;
+if (! function_exists('convertOnOffToBoolean')) {
+    function convertOnOffToBoolean(&$data, $originalKeys)
+    {
+        foreach ($data as $key => $value):
+            if (strtolower($value) == 'on') {
+                $data[$key] = true;
+            }
+        endforeach;
 
-         $differenceKeys = array_diff_key($originalKeys, $data);
-         foreach ($differenceKeys as $key => $value):
-             if (str_starts_with($key, 'is_')) {
-                 $data[$key] = false;
-             }
-         endforeach;
-     }
- }
+        $differenceKeys = array_diff_key($originalKeys, $data);
+        foreach ($differenceKeys as $key => $value):
+            if (str_starts_with($key, 'is_')) {
+                $data[$key] = false;
+            }
+        endforeach;
+    }
+}
